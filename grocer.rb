d5fc59d24acce3e4c0f2a1c0778b364cc7c5e371
Pat McClernan <patmccler@gmail.com>
2020-05-07 08:44:02 -0400

    first test passing

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,32 @@
+require "pry"
 def consolidate_cart(cart)
-  # code here
+
+  result = {}
+
+  cart.each do |item|
+
+    # pull info from cart
+    key = item.keys.first
+    attributes = item[item.keys.first]
+    # binding.pry
+
+    # pull item info from existing results
+    result_for_current_item = result[key]
+
+    if result_for_current_item
+      current_count = result_for_current_item[:count]
+    else
+      current_count = 0
+    end
+
+
+    # save it to results
+    attributes[:count] = current_count + 1
+
+    result[key] = attributes
+  end
+
+  result
 end
 
 def apply_coupons(cart, coupons)
