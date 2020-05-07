b4d20dac71ee81ae6b1533e77aa077027a3c75b5
Idan <idan.zonshein@powerhrg.com>
2020-05-07 09:03:55 -0400

    complete consolidate_cart method

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,18 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+
+  new_cart = {}
+
+  cart.each do |items, count|
+    items.each do |item, hash|
+      new_cart[item] ||= hash
+        new_cart[item][:count] ? new_cart[item][:count] += 1 : new_cart[item][:count] = 1
+    end
+  end
+new_cart
 end
+
 
 def apply_coupons(cart, coupons)
   # code here
